42c2bafb09f830da2917a92776ebfaf9ffd1f236
udoh <66221563+udohahanor@users.noreply.github.com>
2023-01-01 19:20:40 +0100

    favorites tab

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './navbar/Navbar';
 import Carousel from './carousel/Carousel';
 import Gallery from './gallery/Gallery';
+import Favorites from './favorites/Favorites';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       < Carousel />
       <Gallery />
+      < Favorites />
     </>
   );
 }
---
+++ src/favorites/Favorites.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Favorites.css';
+
+const Favorites = () => {
+    return (
+        <div className="favorites-container">
+            Favorites
+        </div>
+    )
+}
+
+export default Favorites;
